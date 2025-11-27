05744a391dae7d3e43955149722322cf2faeaa84
stlin256 <yjzlxljy@outlook.com>
2025-11-27 13:59:40 +0800

    资源本地化

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 def create_app():
 
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='static', static_url_path='/static')
     app.config["TEMPLATES_AUTO_RELOAD"] = True
     
     with app.app_context():
