d68589021c753327b6e482503afe42f558bc412c
stlin256 <yjzlxljy@outlook.com>
2025-11-27 20:11:24 +0800

    加入识别动画

---
+++ app/static/js/weapon-icons.js
@@ -0,0 +1,13 @@
+/* static/js/weapon-icons.js */
+(function() {
+    // 使用 window 挂载，防止重复声明报错，提高兼容性
+    window.WEAPON_ICONS_LIB = {
+        ar: '<path d="M10 35 L10 28 L15 28 L15 22 L85 22 L85 25 L95 25 L95 30 L85 30 L85 35 L75 42 L65 42 L70 35 L55 35 L50 42 L35 42 L40 35 Z M60 22 L60 18 L70 18 L70 22" />',
+        sr: '<path d="M5 32 L15 32 L20 28 L70 28 L70 24 L95 24 L95 27 L70 27 L70 32 L60 40 L50 40 L55 32 L30 32 L25 36 L5 36 Z M35 28 L35 20 L55 20 L55 28" />',
+        smg: '<path d="M15 38 L15 28 L20 28 L20 22 L80 22 L80 32 L70 40 L60 40 L65 32 L50 32 L45 45 L35 45 L40 32 L15 32 Z" />',
+        sg: '<path d="M10 36 L10 30 L90 30 L90 35 L80 40 L70 40 L75 35 L50 35 L50 38 L30 38 L30 35 L10 35 Z M30 35 L30 32 L60 32 L60 35" />',
+        lmg: '<path d="M10 32 L85 28 L85 32 L75 40 L65 40 L70 32 L55 32 L55 40 L40 40 L40 32 L10 32 Z M55 28 L55 20 L40 20 L40 28 M80 32 L85 40" />',
+        hg: '<path d="M30 40 L30 25 L75 25 L75 32 L45 32 L40 40 Z" />',
+        none: '<path d="M10 30 L20 20 L30 40 L40 10 L50 50 L60 20 L70 40 L80 30 L90 30" style="fill:none; stroke-dasharray: 2,2;" />'
+    };
+})();
